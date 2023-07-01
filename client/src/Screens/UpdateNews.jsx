ccbde1cdcca3c61dc4edeb508c5612bbae0b8587
buseilguz <77693665+buseilguz@users.noreply.github.com>
2023-07-01 23:31:13 +0300

    Add files via upload

--- client/src/Screens/UpdateNews.jsx
+++ client/src/Screens/UpdateNews.jsx
@@ -43,7 +43,7 @@ useEffect(()=>{
         const newsId=location.pathname.split("/")[3]
         console.log(newsId)   
       try { 
-        const res=await axios.get("http://localhost:5000/news/getnews/"+newsId)
+        const res=await axios.get("https://reactapp-0xe1.onrender.com/news/getnews/"+newsId)
         setNews(res.data);
         console.log(res)
     } catch (error) {
