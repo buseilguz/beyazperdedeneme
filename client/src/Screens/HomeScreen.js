df4277e8f5259b6ee4ad5cc476208f10626508eb
buseilguz <buse.ilguz@gmail.com>
2023-07-05 12:48:34 +0300

    HomeScreen

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Newses from "./Newses";
+import { useNavigate } from 'react-router-dom';
 
 
 
 const HomeScreen=({user})=>{
+    const navigate=useNavigate()
+    try {
+           
+        navigate("/news/getall/")
+    } catch (err) {
+        console.log(err)
+    }
 
     <Newses/>
    
