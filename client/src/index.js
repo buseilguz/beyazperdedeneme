6d519c772e3ee2706ddfc47e5b4e574ffde2a141
buseilguz <buse.ilguz@gmail.com>
2023-07-10 16:44:11 +0300

    change

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './bootstrap.min.css'
-//import {Auth0Provider} from '@auth0/auth0-react'
+import {Auth0Provider} from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
- // <Auth0Provider domain={process.env.REACT_APP_AUTH0_DOMAIN} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID} redirectUrl='https://reactapp-frontend.onrender.com/'>
+  
     <App />
-  //  </Auth0Provider>
-    
  
 );
 
