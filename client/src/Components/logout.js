962869226e8db2fff5e6b4f5ec2b8a60424e16fa
buseilguz <buse.ilguz@gmail.com>
2023-07-11 13:05:54 +0300

    change

--- client/src/Components/logout.js
+++ client/src/Components/logout.js
@@ -1,4 +1,4 @@
-import { GoogleLogout, GoogleOAuthProvider } from '@react-google-login';
+import { GoogleLogout, GoogleOAuthProvider } from '@react-oauth/google';
 
 const clientId = "730786986543-r0o4f13rg4d53orkufvoq4hk2eah16u7.apps.googleusercontent.com";
 
