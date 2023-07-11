75655f68cfc2a144f58d579dd50d7ab8ed4226ce
buseilguz <buse.ilguz@gmail.com>
2023-07-11 15:08:55 +0300

    c

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-
+import { gapi } from 'gapi-script'
 
 
 const clientId="730786986543-r0o4f13rg4d53orkufvoq4hk2eah16u7.apps.googleusercontent.com";
